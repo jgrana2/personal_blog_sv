a35c916e324beb38e3deb506da3b1f28ee94d77b
Jose Granados <jose@grana2.co>
2024-02-06 00:36:14 -0500

    Add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "../components/Header.svelte";
+    import Introduction from "../components/Introduction.svelte";
+    import GetInTouch from "../components/GetInTouch.svelte";
+    import ProjectList from "../components/ProjectList.svelte";
+    import ArticleList from "../components/ArticleList.svelte";
+    import NewsletterSignUp from "../components/NewsletterSignUp.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    const proyectos = [
+        {
+            title: "PaySubs",
+            year: "2024",
+            description: "A subscription management and payment gateway",
+        },
+        {
+            title: "Mastertrends",
+            year: "2024",
+            description: "Know what's hot and relevant",
+        },
+        // Add more projects here
+    ];
+
+    const reflexiones = [
+        {
+            title: "La verdad sobre el bitcoin",
+            action: "Leer",
+        },
+        {
+            title: "Nada es lo que parece",
+            action: "Leer",
+        },
+        {
+            title: "Complejidad y caos",
+            action: "Leer",
+        },
+        // Add more reflections here
+    ];
+</script>
+
+<div
+    class="flex w-full h-full bg-stone-100 dark:bg-stone-900 text-stone-600 dark:text-stone-300"
+>
+    <div class="flex flex-col max-w-xl mx-auto m-4 p-4">
+        <Header />
+        <Introduction />
+        <GetInTouch />
+        <ProjectList {proyectos} />
+        <ArticleList {reflexiones} />
+        <NewsletterSignUp />
+        <Footer />
+    </div>
+</div>
