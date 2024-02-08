fcdaaa29fdf245f3694e4a7e5bd93e25f548a8e5
Jose Granados <jose@grana2.co>
2024-02-08 11:30:23 -0500

    Edit newsletter sign up form

--- src/components/NewsletterSignUp.svelte
+++ src/components/NewsletterSignUp.svelte
@@ -1,17 +1,10 @@
-<div
-    class="flex flex-col rounded-xl p-4 mt-4 bg-stone-200 p-4 dark:bg-stone-800 mb-4"
->
-    <span class="text-2xl font-bold font-mono"> Newsletter</span>
-    <span class="text-lg pt-4"
-        >Suscríbase abajo para recibir actualizaciones mensuales directo a su
-        bandeja de entrada.</span
-    >
+<div class="flex flex-col rounded-xl p-4 mt-4 bg-stone-200 p-4 dark:bg-stone-800 mb-4">
+    <span class="text-2xl font-bold"> Newsletter</span>
+    <span class="text-lg pt-4">Regístrese aquí para obtener actualizaciones mensuales enviadas directamente a su correo electrónico.</span>
     <form>
         <div class="flex pt-4 gap-2">
-            <input type="text" class="w-full rounded-lg p-2" /><button
-                class="p-4 text-sm bg-stone-200 p-4 dark:bg-stone-700 rounded-lg"
-                >Suscribirse</button
-            >
+            <input type="text" class="w-full rounded-lg p-2" />
+                <button class="p-4 text-sm bg-stone-200 p-4 dark:bg-stone-700 rounded-lg">Registrarse</button>
         </div>
     </form>
 </div>
