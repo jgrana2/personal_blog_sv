edf0fcca1e4594d959e935ee523779eadd87c1c6
Jose Granados <jose@grana2.co>
2024-02-08 11:26:39 -0500

    Change articles to posts

--- src/components/Posts.svelte
+++ src/components/Posts.svelte
@@ -1,19 +1,21 @@
 <script>
     import { ChevronRight } from "svelte-heros-v2";
-    export let entradas = [];
+    export let posts = [];
 </script>
 
 <div class="flex justify-between items-center py-4 mt-4 border-b">
-    <span class="text-2xl font-bold font-mono"> Entradas</span>
-    <button class="flex items-center hover:underline">
-        <span class="text-base">Ver todo </span><ChevronRight size="15" />
-    </button>
+    <span class="text-2xl font-bold"> Entradas</span>
+    <a href="posts">
+        <button class="flex items-center hover:underline">
+            <span class="text-base">Ver todo </span><ChevronRight size="15" />
+        </button>
+    </a>
 </div>
 
-{#each entradas as entrada}
+{#each posts as entrada}
     <div class="flex flex-col py-4 border-b">
         <div class="flex justify-between items-center">
-            <span class="font-mono">{entrada.title}</span>
+            <span class="">{entrada.title}</span>
             <button class="flex items-center hover:underline">
                 <span class="text-base">{entrada.action} </span><ChevronRight
                     size="15"
---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import ProjectList from "../../components/ProjectList.svelte";
+    import NewsletterSignUp from "../../components/NewsletterSignUp.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import Profile from "../../components/Profile.svelte";
+    import Posts from "../../components/Posts.svelte";
+
+    const posts = [
+        {
+            title: "La verdad sobre el bitcoin",
+            action: "Leer",
+        },
+        {
+            title: "Nada es lo que parece",
+            action: "Leer",
+        },
+        {
+            title: "Complejidad y caos",
+            action: "Leer",
+        },
+        {
+            title: "Semiconductor news",
+            action: "Leer",
+        },
+        {
+            title: "Introduction to LLMs",
+            action: "Leer",
+        },
+        {
+            title: "SvelteKit + Tailwind CSS for the win",
+            action: "Leer",
+        },
+        // Add more reflections here
+    ];
+</script>
+
+<div
+    class="flex w-full h-full bg-stone-100 dark:bg-stone-900 text-stone-600 dark:text-stone-300"
+>
+    <div class="flex flex-col max-w-xl mx-auto m-4 p-4">
+        <Profile />
+        <Posts {posts} />
+        <NewsletterSignUp />
+        <Footer />
+    </div>
+</div>
