b3e0d55043acd7545e5b8bee94e6ebafd26c9b24
Jose Granados <jose@grana2.co>
2024-02-18 09:18:04 -0500

    Change page name

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
 <div class="flex w-full border-t justify-between text-xs pt-4">
-    <span>&#169; 2025 - Jose Granados</span>
+    <span>&#169; 2025 - Grana2 Embedded</span>
     <div> <a href="/"><button class="hover:underline">Inicio</button></a> / <a href="projects"><button class="hover:underline">Proyectos</button></a> / <a href="posts"><button class="hover:underline">Entradas</button></a> / <a href="contact"><button class="hover:underline">Contacto</button></a></div>
 </div>
--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,3 +1,3 @@
 <div class="flex items-center w-full">
-    <span class="text-2xl font-extralight py-4"> JOSE GRANADOS </span>
+    <span class="text-2xl font-extralight py-4"> Grana2 Embedded </span>
 </div>
