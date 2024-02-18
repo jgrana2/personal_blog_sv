ed64ade121a5562fabe60a2758d4f2f5c1921c7e
Jose Granados <jose@grana2.co>
2024-02-17 21:16:55 -0500

    Add list management

--- src/components/NewsletterSignUp.svelte
+++ src/components/NewsletterSignUp.svelte
@@ -1,10 +1,31 @@
-<div class="flex flex-col rounded-xl p-4 mt-4 bg-stone-200 p-4 dark:bg-stone-800 mb-4">
+<div
+    class="flex flex-col rounded-xl p-4 mt-4 bg-stone-200 dark:bg-stone-800 mb-4"
+>
     <span class="text-2xl font-bold"> Newsletter</span>
-    <span class="text-lg pt-4">Regístrese aquí para obtener actualizaciones mensuales enviadas directamente a su correo electrónico.</span>
-    <form>
+    <span class="text-lg pt-4"
+        >Regístrese aquí para obtener actualizaciones mensuales enviadas
+        directamente a su correo electrónico.</span
+    >
+    <form
+                action="https://grana2.us21.list-manage.com/subscribe/post?u=2d41a5a81e9c3fc434adc07a0&amp;id=624caf0680&amp;f_id=00abf8e6f0"
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                class="validate"
+                target="_self"
+                novalidate=""
+            >
         <div class="flex pt-4 gap-2">
-            <input type="text" class="w-full rounded-lg p-2" />
-                <button class="p-4 text-sm bg-stone-200 p-4 dark:bg-stone-700 rounded-lg">Registrarse</button>
+            <input type="email"
+            name="EMAIL"
+            class="required email w-full rounded-lg p-2 text-stone-800"
+            id="mce-EMAIL"
+            required=""
+            value="" />
+            <button
+                class="p-4 text-sm bg-stone-200 dark:bg-stone-700 rounded-lg"
+                >Registrarse</button
+            >
         </div>
     </form>
 </div>
