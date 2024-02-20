df2b0bb4fe18cb958dcf3b804f42137a88aea6bc
Jose Granados <jose@grana2.co>
2024-02-19 21:06:18 -0500

    Fix post dates

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -2,15 +2,30 @@
     export let post = {};
 
     function formatDate(dateString) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return new Date(dateString).toLocaleDateString('es-ES', options);
+        const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+        };
+        return new Date(dateString).toLocaleDateString("es-ES", options);
     }
 </script>
 
 <div class="flex flex-col">
     <!-- Title -->
     <h1 class="text-lg font-extrabold mt-2">{post.title}</h1>
-    <span class="mb-4 text-xs">Publicado el {formatDate(post.created_at)}</span>
+    {#if post.created_at}
+        <span class="text-xs mt-4"
+            >Publicado el {formatDate(post.created_at)}</span
+        >
+    {/if}
+    {#if post.updated_at}
+        <span class="mb-4 text-xs"
+            >Actualizado el {formatDate(post.updated_at)}</span
+        >
+    {/if}
 
     <!-- Content -->
     <div>
