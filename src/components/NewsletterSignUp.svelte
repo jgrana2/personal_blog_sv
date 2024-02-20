b85aa2f5c9c93c4b57a0ca0a8bd82c69062f89b2
Jose Granados <jose@grana2.co>
2024-02-19 22:26:56 -0500

    Add contacts to database

--- src/components/NewsletterSignUp.svelte
+++ src/components/NewsletterSignUp.svelte
@@ -1,30 +1,89 @@
+<script>
+    import { checkContactExists, submitContact, updateContact } from "$lib/appwrite";
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const email = formData.get("email");
+
+        if (!email.trim()) {
+            console.error("Se requiere el correo electrónico");
+            dispatch("submitError", {
+                message: "Se requiere el correo electrónico",
+            });
+            return;
+        }
+
+        try {
+            let response;
+
+            // Check if contact already exists
+            const contactId = await checkContactExists(email);
+
+            // Returned value from 'checkContactExists' will be stored in 'contactId'
+            if (contactId) {
+                // If contact exists, update the contact information, the email should be the same
+                response = await updateContact(contactId, email);
+                console.log("Contacto actualizado con éxito");
+
+                // Dispatch event indicating success with the response object and a message
+                dispatch("updateSuccess", {
+                    response, // contains the returned value from 'updateContact'
+                    message: "Contacto actualizado con éxito",
+                });
+
+                alert("Contacto actualizado con éxito");
+            } else {
+                // If contact does not exist, submit as new contact
+                response = await submitContact({ email });
+                console.log("Suscripción confirmada");
+
+                // Dispatch event indicating success with the response object and a message
+                dispatch("submitSuccess", {
+                    response, // contains the returned value from 'submitContact'
+                    message: "Suscripción confirmada",
+                });
+
+                // Notify the user about the confirmation
+                alert("Suscripción confirmada");
+
+                // Reset the form
+                event.target.reset();
+            }
+        } catch (error) {
+            // Log the error and notify the user
+            console.error("Subscription failed", error);
+            alert("Suscripción fallida");
+
+            // Dispatch event indicating an error with the error object
+            dispatch("submitError", { error });
+        }
+    }
+</script>
+
 <div
     class="flex flex-col rounded-xl p-4 mt-4 bg-stone-200 dark:bg-stone-800 mb-4"
 >
-    <span class="text-2xl font-bold"> Newsletter</span>
+    <span class="text-2xl font-bold">Lista de correo</span>
     <span class="text-lg pt-4"
         >Regístrese aquí para obtener actualizaciones mensuales enviadas
         directamente a su correo electrónico.</span
     >
-    <form
-                action="https://grana2.us21.list-manage.com/subscribe/post?u=2d41a5a81e9c3fc434adc07a0&amp;id=624caf0680&amp;f_id=00abf8e6f0"
-                method="post"
-                id="mc-embedded-subscribe-form"
-                name="mc-embedded-subscribe-form"
-                class="validate"
-                target="_self"
-                novalidate=""
-            >
+    <form on:submit={handleSubmit}>
         <div class="flex pt-4 gap-2">
-            <input type="email"
-            name="EMAIL"
-            class="required email w-full rounded-lg p-2 text-stone-800"
-            id="mce-EMAIL"
-            required
-            value="" />
+            <input
+                type="email"
+                name="email"
+                class="required email w-full rounded-lg p-2 text-stone-800"
+                id="mce-EMAIL"
+                required
+            />
             <button
                 class="p-4 text-sm bg-stone-200 dark:bg-stone-700 rounded-lg"
-                >Registrarse</button
+                type="submit">Registrarse</button
             >
         </div>
     </form>
--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -5,12 +5,13 @@ const DATABASE_ID = '65d11e8af3d5fd8ba809'; // Replace with your database ID
 const PROJECTS_COLLECTION_ID = '65d11e9541615c7725ec'; // Replace with your collection ID
 const POSTS_COLLECTION_ID = '65d11e9c666346a21cab'; // Replace with your collection ID
 const MESSAGES_COLLECTION_ID = '65d21d60410fbdde0137'; // Replace with your collection ID
+const CONTACTS_COLLECTION_ID = '65d40e353fc388adb189'; // Replace with your collection ID
 
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(PROJECT_ID); // Replace with your project ID
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(PROJECT_ID); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
@@ -57,5 +58,48 @@ export async function submitMessage(message) {
         MESSAGES_COLLECTION_ID,
         ID.unique(),
         message
+    );
+}
+
+export async function submitContact(email) {
+    return await databases.createDocument(
+        DATABASE_ID,
+        CONTACTS_COLLECTION_ID,
+        ID.unique(),
+        email
+    );
+}
+
+export async function checkContactExists(email) {
+    try {
+        const response = await databases.listDocuments(
+            DATABASE_ID,
+            CONTACTS_COLLECTION_ID,
+            [
+                Query.equal('email', email),
+            ]
+        );
+
+        // If a contact is found, return the $id of the first matching document
+        if (response.documents.length > 0) {
+            return response.documents[0].$id; // Assuming that $id is the field with the document id
+        } else {
+            // Return null or however you wish to indicate that the contact does not exist
+            return null;
+        }
+
+    } catch (error) {
+        console.error("Error: el contacto ya existe", error);
+        alert("Error: el contacto ya existe");
+        throw new Error("Failed to check if contact exists");
+    }
+}
+
+export async function updateContact(id, email) {
+    return await databases.updateDocument(
+        DATABASE_ID,
+        CONTACTS_COLLECTION_ID,
+        id,
+        {email}
     );
 }
