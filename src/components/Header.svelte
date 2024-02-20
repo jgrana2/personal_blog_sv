000e75fb932197f16c6b8c610302d31717955d5e
Jose Granados <jose@grana2.co>
2024-02-19 21:06:46 -0500

    Add coming soon message

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,13 @@
 <script>
     // Header specific scripts can go here if needed
+
+
+    function handleEnglish(event) {
+        alert("Coming soon");
+    }
 </script>
 
 <div class="flex justify-end text-xs gap-2">
     <span class="no-underline">Español</span>
-    <a href="#" class="underline">English</a>
+    <button class="underline" on:click={handleEnglish}>English</button>
 </div>
